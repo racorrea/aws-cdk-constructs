36191bcc503cb3f72479ba3aea06a41a2c2f246c
Roddy Correa Tenesaca <ra.correa90@gmail.com>
2023-01-10 10:25:14 -0500

    tags by stack added

--- bin/app.ts
+++ bin/app.ts
@@ -3,15 +3,18 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Stack } from '../lib/stack';
 import { SpaStack } from '../lib/spa-stack';
+import { TestStack } from '../lib/test-stack';
+import { CfnOutput, DefaultStackSynthesizer, Stage } from 'aws-cdk-lib';
+import { Role } from 'aws-cdk-lib/aws-iam';
 
 // require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` })
-const environment="dev"
+const environment = "dev"
 require('dotenv').config({ path: `./.env.${environment}` })
 
 const app = new cdk.App();
 const env = {
-    account: process.env.AWS_ACCOUNT_ID ?? process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.AWS_PRIMARY_REGION ?? process.env.CDK_DEFAULT_REGION
+  account: process.env.AWS_ACCOUNT_ID ?? process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.AWS_PRIMARY_REGION ?? process.env.CDK_DEFAULT_REGION
 }
 
 const stackName = `${process.env.NAME}-stack-${environment}`;
@@ -20,16 +23,29 @@ if (!process.env?.CERTIFICATE_ARN) {
   throw new Error('Could not resolve certificate. Please pass it with the CERTIFICATE_ARN environment variable.');
 }
 
-console.log("ENV: ");
-console.log(env);
-
-
-new Stack(app, stackName, {
-  stackName: `${process.env.NAME}-${environment}`,
+const testStack = new TestStack(app, stackName, {
+  stackName: stackName,
   env: env,
+  tags:{
+    environment: 'dev',
+    project: 'poc-aws-cdk',
+  },
+  synthesizer: new DefaultStackSynthesizer({
+    fileAssetsBucketName: `cdk-${stackName}-${env.region}-assets`,
+
+  }),
 });
 
-new SpaStack(app, `${process.env.NAME}-spa-${environment}`, {
-  stackName: `${process.env.NAME}-spa-${environment}`,
-  env: env,
-});
+new Stage(app, 'DevStage');
+
+
+
+// new Stack(app, stackName, {
+//   stackName: `${process.env.NAME}-${environment}`,
+//   env: env,
+// });
+
+// new SpaStack(app, `${process.env.NAME}-spa-${environment}`, {
+//   stackName: `${process.env.NAME}-spa-${environment}`,
+//   env: env,
+// });
