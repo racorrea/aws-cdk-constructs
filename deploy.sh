c44877e11c9a2abe330dfa3d594e6ebe56c0ebf3
Roddy Correa Tenesaca <ra.correa90@gmail.com>
2023-01-10 11:01:18 -0500

    deploy modified for multstage deployment

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,10 @@ export AWS_REGION=us-east-1
 rm -rf ./cdk.out
 npm run build
 cdk synth --all
-cdk deploy --all
+
+#Deploy only defined stack
+cdk deploy aws-cdk-constructs-stack-dev-dev
+
+
+#Deploy all Stacks
+#cdk deploy --all
